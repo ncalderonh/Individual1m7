0c102b38db44987a5d8f855287ac1e3918599944
Nicolás Calderón Hernández <calderonhernandez.nicolas@gmail.com>
2023-06-27 11:07:53 -0400

    Update settings.py

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -25,7 +25,7 @@ PROJECT_APP_PATH = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4h+89gjkt(0uxx6c0+e$xkqxq^u*h30jos!b0fpn7c+(*a*#@k'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
